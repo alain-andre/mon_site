949b1046c3509874f60663e1c06ff8b879e7e05d
Alain ANDRE <wordsbybird@gmail.com>
2015-05-22 15:09:20 +0200

    oops _assets

--- octopress/_plugins/lunr_search.rb
+++ octopress/_plugins/lunr_search.rb
@@ -60,8 +60,8 @@ module Jekyll
       # File I/O: create search.json file and write out pretty-printed JSON
       filename = 'search.json'
       
-      puts "====== writting #{filename} to #{site.source}"
-      File.open(File.join(site.source, 'assets', filename), "w") do |file|
+      puts "====== writting #{filename} to #{site.source}/_assets"
+      File.open(File.join(site.source, '_assets', filename), "w") do |file|
         file.write(json)
       end
 
