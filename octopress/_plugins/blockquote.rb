4ce9ec8fec9e99282a38c58245d9c0563d92e99d
Alain ANDRE <wordsbybird@gmail.com>
2015-06-12 17:21:49 +0200

    Passe à require_relative

--- octopress/_plugins/blockquote.rb
+++ octopress/_plugins/blockquote.rb
@@ -14,7 +14,7 @@
 #     <strong>Bobby Willis</strong><cite><a href="http://google.com/search?q=pants">The Search For Bobby's Pants</a>
 #   </blockquote>
 #
-require 'titlecase.rb'
+require_relative 'titlecase'
 
 module Jekyll
 
